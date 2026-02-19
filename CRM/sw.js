6d2717966bbf4e8dcc8421d70d999ee31558f847
mannyserrano4-oss <mannyserrano4@gmail.com>
2026-02-19 10:14:36 -0500

    Create sw.js

---
+++ CRM/sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'blackbook-v1';
+
+// Files to save for offline use
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon.png',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// Install Event: Saves the files to the cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Fetch Event: Loads files from cache if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version if found
+        if (response) {
+          return response;
+        }
+        // Otherwise fetch from the internet
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate Event: Cleans up old caches when you update the app
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
