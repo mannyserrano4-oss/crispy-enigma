74b1831b36127e8f84384d665f9c30da9f76f569
mannyserrano4-oss <mannyserrano4@gmail.com>
2026-07-11 15:54:44 -0400

    Update sw.js

--- clip/sw.js
+++ clip/sw.js
@@ -1,29 +1,22 @@
-const CACHE_NAME = 'snippet-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json'
-];
+const CACHE_NAME = 'snippet-cache-v2';
+const urlsToCache = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
-// Install event: cache the files
 self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
   );
 });
 
-// Fetch event: serve from cache if offline
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Return cached file if found, otherwise go to the network
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
